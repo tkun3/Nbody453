29ffaec3fa09667d15696630d4799ba0a43018b2
tkun3 <tapimlott@live.ca>
2018-02-19 12:13:50 -0800

    added comments

--- simulation.c
+++ simulation.c
@@ -9,6 +9,7 @@
 
 
 
+//node struct to represent each planetary body / node
 struct Node {
 	double mass;
 	double x;
@@ -33,9 +34,9 @@ double simTime = 50000;
 int    nodeIndexes;
 struct Node nodeData[28];
 struct Node tempNodeData[28];
-char *fileName = "nbody_initial_new.txt";
+char *fileName = "nbody_initial_new.txt"; //default file name
 double forces[3];
-double G = 6.67408E-11;
+double G = 6.67408E-11;	//G constant
 double x, y, z, mass, vX, vY, vZ;
 
 
@@ -107,6 +108,7 @@ double updateNode(){
 
 
 double getForce(int b, int c, double radius){
+	//Equation for newtons law of universal gravitation
 	double force = (G * nodeData[b].mass * nodeData[c].mass) / (radius * radius);
 	return force;
 }
@@ -114,6 +116,7 @@ double getForce(int b, int c, double radius){
 
 
 double updateVelocity(int b, double fx, double fy, double fz){
+	//add the resulting velocity change to a temporary array from the force acting on the node
 	tempNodeData[b].vX += fx/nodeData[b].mass * timeDelta;
 	tempNodeData[b].vY += fy/nodeData[b].mass * timeDelta;
 	tempNodeData[b].vZ += fz/nodeData[b].mass * timeDelta;
@@ -123,20 +126,27 @@ double updateVelocity(int b, double fx, double fy, double fz){
 
 
 double calculateForce(int b, int c){
+	//calculate the distance between node b and c
 	double distance = sqrt(pow((nodeData[c].x - nodeData[b].x),2.00) + pow((nodeData[c].y - nodeData[b].y),2.00) + pow((nodeData[c].z - nodeData[b].z),2.00));
+	//Ignore force calculations if the two nodes are too close
 	if (distance < 1000.00){
 		return 0.00;
 	}else{
+		//calculate force acting on each node
 		double force = getForce(b,c,distance);
+		//determine the direction vector that the force has
 		double directionVectorX = (nodeData[c].x - nodeData[b].x) / distance;
 		double directionVectorY = (nodeData[c].y - nodeData[b].y) / distance;
 		double directionVectorZ = (nodeData[c].z - nodeData[b].z) / distance;
+		//multiply fore bye direction vector to determine the force component in each direction
 		double fx = directionVectorX * force;
 		double fy = directionVectorY * force;
 		double fz = directionVectorZ * force;
+		//iteratively add the change of position due to each node to node force interactiona to a temporary NodeData array
 		tempNodeData[b].x += 0.5 * fx/nodeData[b].mass * timeDelta * timeDelta;
 		tempNodeData[b].y += 0.5 * fy/nodeData[b].mass * timeDelta * timeDelta;
 		tempNodeData[b].z += 0.5 * fz/nodeData[b].mass * timeDelta * timeDelta;
+		//update the velocity from the effects of node acceleration
 		updateVelocity(b, fx, fy, fz);
 		return 0.00;
 	}
@@ -153,14 +163,20 @@ double printResults(){
 
 double startSimulation(){
 	double time = 0.00;
+	//execute simulation while current time is less than simulation time
 	while(time < simTime){
+		//iterate over each node for N-1 nodes to calculate force effects of one another
 		for(int b = 0; b < nodeIndexes; b++){
 			for(int c = 0; c < nodeIndexes; c++){
+				//calculate force for nodes that are not itself
 				if(c != b){
 					calculateForce(b, c);
 				}
 			}
 		}
+		//update parameters of all nodes after completion of calculating all
+		//new parameters from node force effects / velocities on one another
+		//(Executes once every time delta)
 		updateNode();
 		time += timeDelta;
 	}
@@ -171,6 +187,7 @@ double startSimulation(){
 
 
 int main(int argc, char *argv[]) {
+	//set global parameters based on arguments
 	simTime = atof(argv[1]);
 	fileName = argv[2];
 	initializeNodes();
